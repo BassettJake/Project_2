6255d9983d1ffe1575903d61df8db23c32bc3a5e
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-09 20:26:19 -0700

    init

--- server.js
+++ server.js
@@ -36,23 +36,20 @@ app.get('/viewCharacters', function (req, res) {
     console.log("Back from DB with result: ");
     console.log(result.rows);
 
-    var html = '<ul>';
+    var html = "";
     for(i in result.rows){
       console.log(result.rows[i]);
       for(j in result.rows[i]){
         console.log(result.rows[i][j]);
         html += '<li>' + result.rows[i][j] + '</li>';
       }
-
     }
-
     html += '</ul>';
 
-    var params = {
-      dbResult: html
-    }
-
-    res.render('pages/viewCharacters', params);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(html);
+    res.render('pages/viewCharacters');
+    res.end();
   });
 });
 app.use(express.static(path.join(__dirname, 'public')));
