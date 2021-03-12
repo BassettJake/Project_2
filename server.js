fa21599b909e37c72126f869fe99c719b94c2f87
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-12 11:51:34 -0700

    db

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const charRes = function(callback){
 
 app.get('/viewCharacters', (req, res) => {
 charRes(function(err, result){return result});
+console.log(charRes, charRes.result);
   res.send(charRes);
 
 
