34142ebf829854c50576f112505a61bd43da07da
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-12 10:55:21 -0700

    db

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/viewCharacters', (req, res) => {
   console.log("???????????????????");
 });
 
-app.get('viewCharacters', (req, res) => {
+app.get('/viewCharacters', (req, res) => {
   var params = {
     dbResult: "testing"
   }
