ddacef80a0a6928b59148bf4e4ef92d5f9e7d184
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-13 11:49:53 -0700

    css

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get('/viewCharacters', (req, res) => {
   });
 });
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
