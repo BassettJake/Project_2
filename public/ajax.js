d46e30f25fdef8a650e024598362512fb350332d
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-13 12:14:17 -0700

    css

--- public/ajax.js
+++ public/ajax.js
@@ -1,35 +1,61 @@
-$(function(){
-    $('#submitButton').on('click', function(){
-      $.ajax({
-        url: '/viewCharacters',
-        contentType: 'application/json',
-        success: function(res){
-          var html = '<section class="characterWrapper">';
-          for(i in res){
-            html += '<section class="character">';
-            html += '<section class="top">';
-            html += '<section class="char-name">' + res[i]['name'] + '</section>';
-            html += '<section class="top-seg">';
-            html += '<section class="char-medText char-class">' + res[i]['class'] + '</section>';
-            html += '<section class="char-medText char-species">' + res[i]['species'] + '</section>';
-            html += '<section class="char-medText char-weapon">' + res[i]['weapon'] + '</section>';
-            html += '<section class="char-medText char-armor">' + res[i]['armor'] + '</section>';
-            html += '</section>';
-            html += '</section>';
-            html += '<section class="mid">';
-            html += '<section class="char-medText char-strength">' + res[i]['strength'] + '</section>';
-            html += '<section class="char-medText char-agility">' + res[i]['agility'] + '</section>';
-            html += '<section class="char-medText char-wisdom">' + res[i]['wisdom'] + '</section>';
-            html += '<section class="char-medText char-intelligence">' + res[i]['intelligence'] + '</section>';
-            html += '</section>';
-            html += '<section class="bottom">';
-            html += '<section class="char-backstory">' + res[i]['backstory'] + '</section>';
-            html += '</section>';
-            html += '</section>';
-          }
+$(function () {
+  $('#submitButton').on('click', function () {
+    $.ajax({
+      url: '/viewCharacters',
+      contentType: 'application/json',
+      success: function (res) {
+        var html = '<section class="characterWrapper">';
+        for (i in res) {
+          html += '<section class="character">';
+          html += '<section class="top">';
+          html += '<section class="char-name">' + res[i]['name'] + '</section>';
+          html += '<section class="top-seg">';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Species</section>';
+          html += '<section class="char-medText char-species">' + res[i]['species'] + '</section>';
           html += '</section>';
-          document.getElementById("homeWrapper").innerHTML = html;
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Class</section>';
+          html += '<section class="char-medText char-class">' + res[i]['class'] + '</section>';
+          html += '</section>';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Weapon</section>';
+          html += '<section class="char-medText char-weapon">' + res[i]['weapon'] + '</section>';
+          html += '</section>';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Armor</section>';
+          html += '<section class="char-medText char-armor">' + res[i]['armor'] + '</section>';
+          html += '</section>';
+          html += '</section>';
+          html += '</section>';
+          html += '<hr>';
+          html += '<section class="mid">';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Strength</section>';
+          html += '<section class="char-medText char-strength">' + res[i]['strength'] + '</section>';
+          html += '</section>';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Agility</section>';
+          html += '<section class="char-medText char-agility">' + res[i]['agility'] + '</section>';
+          html += '</section>';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Wisdom</section>';
+          html += '<section class="char-medText char-wisdom">' + res[i]['wisdom'] + '</section>';
+          html += '</section>';
+          html += '<section class="labelWrap">';
+          html += '<section class="label">Intelligence</section>';
+          html += '<section class="char-medText char-intelligence">' + res[i]['intelligence'] + '</section>';
+          html += '</section>';
+          html += '</section>';
+          html += '<hr>';
+          html += '<section class="bottom">';
+          html += '<section class="char-backstory">' + res[i]['backstory'] + '</section>';
+          html += '</section>';
+          html += '</section>';
         }
-      });
+        html += '</section>';
+        document.getElementById("homeWrapper").innerHTML = html;
+      }
     });
   });
+});
