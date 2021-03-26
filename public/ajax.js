0fd7dd48c6c94895d9614ef004de4b48053866ea
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-03-26 13:12:02 -0600

    validation

--- public/ajax.js
+++ public/ajax.js
@@ -163,7 +163,7 @@ function toBackstory() {
 function toStats() {
   var eleBackstory = (document.getElementById("backstory").value).trim();
   var message = "";
-  if (eleBackstory.match("/^[a-z ,.'!()-]+$/gi")) {
+  if (eleBackstory.match("/^[a-z ,.'\!\(\)\-]+$/gi")) {
     console.log( document.getElementById("backstory").value);
   setCharDetails("backstory", document.getElementById("backstory").value);
 
@@ -176,7 +176,7 @@ function toStats() {
     });
   } else {
     message += "<ul>";
-    if (!(eleBackstory.match("/^[a-z ,.'!()-]+$/gi"))) {
+    if (!(eleBackstory.match("/^[a-z ,.'\!\(\)\-]+$/gi"))) {
       message += "<li>Backstory can only include letters, spaces, commas, exclaimations, parenthesis, apostrophes, periods, and hyphens.</li>";
     }
     message += "</ul>";
